b549adf8aa5d8fdb3afe43400b4d6056c3237033
Jamie Gardner <jamie.gardner@civica.co.uk>
2019-08-05 20:20:18 +0100

    Style basic app

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+final Color primary      = Color(0xff024873);
+final Color primaryMuted = Color(0xff024059);
+final Color primaryDark  = Color(0xff011c26);
+final Color accent       = Color(0xfff29f80);
+final Color accentLight  = Color(0xfff2f2f2);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AI Horoscopes',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: MyHomePage(title: 'AI Horoscopes'),
     );
   }
@@ -31,13 +34,21 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        backgroundColor: primary,
+        elevation: 0,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('No more cards!',),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            stops: [0.0, 0.06, 0.9],
+            colors: [
+              Colors.black,
+              primaryDark,
+              primary
+            ],
+          )
         ),
       ),
     );
